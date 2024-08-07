08c9065cf44a5c05673deca25eb53946d46cdd60
jiangtao <jiangtao.cctv@qq.com>
2024-08-07 09:25:44 +0800

    chore: 更新版本

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,10 +1,10 @@
 // 调整umi 默认配置
 import React from "react";
-import {appendDefaultRoutes, cacheRoutes} from "@tmgg/tmgg-system";
+import {patchRoutesDependency} from "@tmgg/tmgg-system/src/common/system/lib-route";
+import {getRoutes} from "@tmgg/tmgg-system";
 
 export function patchRoutes({ routes }) {
-  appendDefaultRoutes(routes)
-  cacheRoutes(routes)
+  patchRoutesDependency(routes, getRoutes())
 }
 
 
